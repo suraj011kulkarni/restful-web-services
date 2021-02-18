1afa47dd44e15ae92b3d0f2676e30d11801fa3c4
suraj kulkarni <suraj.kulkarni@smartcloudtek.com>
2021-02-18 12:49:35 +0530

    User curd operation

---
+++ src/main/java/com/snk/rest/webservices/restfulwebservice/users/domain/User.java
@@ -0,0 +1,55 @@
+package com.snk.rest.webservices.restfulwebservice.users.domain;
+
+import java.util.Date;
+
+public class User {
+
+
+    private Long id;
+    private String name;
+    private Date dob;
+
+    public User() {
+    }
+
+
+    public User(Long id, String name, Date dob) {
+        this.id = id;
+        this.name = name;
+        this.dob = dob;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dob=" + dob +
+                '}';
+    }
+}
